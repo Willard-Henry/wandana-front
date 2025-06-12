13c8aea545e739b8912217b41a757b617cce0bec
nancy donkoh <ewurabanancy05@gmail.com>
2025-06-12 18:26:12 +0000

    Save my local changes before pulling

--- App.js
+++ App.js
@@ -1,3 +1,105 @@
+// import * as React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { Ionicons } from "@expo/vector-icons";
+
+// import HomeScreen from "./screens/HomeScreen";
+// import CategoryScreen from "./screens/CategoryScreen";
+// import TrendsScreen from "./screens/TrendsScreen";
+// import CartScreen from "./screens/CartScreen";
+// import MeScreen from "./screens/MeScreen";
+// import LoginScreen from "./screens/LoginScreen";
+// import SignupScreen from "./screens/SignupScreen";
+// import WelcomeScreen from "./screens/WelcomeScreen";
+// import WishlistScreen from "./screens/WishlistScreen";
+// import NotificationScreen from "./screens/NotificationScreen";
+
+// const Tab = createBottomTabNavigator();
+// const Stack = createNativeStackNavigator();
+
+// function MainTabs() {
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Home"
+//       screenOptions={({ route }) => ({
+//         headerShown: false,
+//         tabBarIcon: ({ focused, color, size }) => {
+//           let iconName;
+//           switch (route.name) {
+//             case "Home":
+//               iconName = focused ? "home" : "home-outline";
+//               break;
+//             case "Category":
+//               iconName = focused ? "list" : "list-outline";
+//               break;
+//             case "Trends":
+//               iconName = focused ? "trending-up" : "trending-up-outline";
+//               break;
+//             case "Cart":
+//               iconName = focused ? "cart" : "cart-outline";
+//               break;
+//             case "Me":
+//               iconName = focused ? "person" : "person-outline";
+//               break;
+//             default:
+//               iconName = "ellipse";
+//           }
+//           return <Ionicons name={iconName} size={size} color={color} />;
+//         },
+//         tabBarActiveTintColor: "#7f00ff",
+//         tabBarInactiveTintColor: "#7F55B1",
+//       })}
+//     >
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Category" component={CategoryScreen} />
+//       <Tab.Screen name="Trends" component={TrendsScreen} />
+//       <Tab.Screen name="Cart" component={CartScreen} />
+//       <Tab.Screen name="Me" component={MeScreen} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// export default function App() {
+//   const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator screenOptions={{ headerShown: false }}>
+//         //{isAuthenticated ? (
+//           <>
+//             <Stack.Screen name="MainTabs" component={MainTabs} />
+//             <Stack.Screen name="Wishlist" component={WishlistScreen} />
+//             <Stack.Screen name="Notification" component={NotificationScreen} />
+//           </>
+//         ) : (
+//           <>
+//             <Stack.Screen name="Login">
+//               {(props) => (
+//                 <LoginScreen
+//                   {...props}
+//                   onLogin={() => setIsAuthenticated(true)}
+//                 />
+//               )}
+//             </Stack.Screen>
+//             <Stack.Screen name="Signup" component={SignupScreen} />
+//             <Stack.Screen name="Welcome">
+//               {(props) => (
+//                 <WelcomeScreen
+//                   {...props}
+//                   setIsAuthenticated={setIsAuthenticated}
+//                 />
+//               )}
+//             </Stack.Screen>
+//           </>
+//         )}
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -9,9 +111,6 @@ import CategoryScreen from "./screens/CategoryScreen";
 import TrendsScreen from "./screens/TrendsScreen";
 import CartScreen from "./screens/CartScreen";
 import MeScreen from "./screens/MeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import SignupScreen from "./screens/SignupScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
 import WishlistScreen from "./screens/WishlistScreen";
 import NotificationScreen from "./screens/NotificationScreen";
 
@@ -61,38 +160,12 @@ function MainTabs() {
 }
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {isAuthenticated ? (
-          <>
-            <Stack.Screen name="MainTabs" component={MainTabs} />
-            <Stack.Screen name="Wishlist" component={WishlistScreen} />
-            <Stack.Screen name="Notification" component={NotificationScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login">
-              {(props) => (
-                <LoginScreen
-                  {...props}
-                  onLogin={() => setIsAuthenticated(true)}
-                />
-              )}
-            </Stack.Screen>
-            <Stack.Screen name="Signup" component={SignupScreen} />
-            <Stack.Screen name="Welcome">
-              {(props) => (
-                <WelcomeScreen
-                  {...props}
-                  setIsAuthenticated={setIsAuthenticated}
-                />
-              )}
-            </Stack.Screen>
-          </>
-        )}
+        <Stack.Screen name="MainTabs" component={MainTabs} />
+        <Stack.Screen name="Wishlist" component={WishlistScreen} />
+        <Stack.Screen name="Notification" component={NotificationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
